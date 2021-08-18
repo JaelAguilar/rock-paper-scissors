0253609a3ecabbd7ca846b520089354506b9bf45
Jael Aguilar <cjaelaguilar@gmail.com>
2021-08-18 08:11:10 -0500

    Added documentation

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 let options = ["rock", "paper", "scissors"]
 
+/**
+ *
+ *
+ * @return {int} The random result from 0 to 2 that corresponds to the choosed option of the computer
+ */
 function computerPlay() {
   choosed = Math.floor(Math.random() * 3)
   return choosed
 }
-
+/**
+ * 
+ * @param {string} playerSelection 
+ * @param {int} computerSelection 
+ * @returns {string} result of the round
+ */
 function playRound(playerSelection, computerSelection) {
+    /*playerSelection is turned to a number between 0 and 2, if commputerSelection is the same, it's a tie, if it is the next, it's a lost, and if it is the previous, it's a win :)*/
     if (computerSelection == options.indexOf(playerSelection)) {
         return("It's a tie!")
     }
