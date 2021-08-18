f4a9f181e774ac3811746f355d5741797f4a0624
Jael Aguilar <cjaelaguilar@gmail.com>
2021-08-17 22:23:05 -0500

    Javascript code for game algorithm

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+let options = ["rock", "paper", "scissors"]
+
 function computerPlay() {
-    options=["Rock","Paper", "Scissors"]
-    choosed = Math.floor(Math.random() * 3)
-    return options[choosed]
+  choosed = Math.floor(Math.random() * 3)
+  return choosed
 }
 
-console.log(computerPlay())
+function getResult(playerSelection, computerSelection) {
+    if (computerSelection == options.indexOf(playerSelection)) {
+        return("It's a tie!")
+    }
+    else if (computerSelection == (options.indexOf(playerSelection)+1)%3) {
+        return("You lost!")
+    }
+    else if (computerSelection == (options.indexOf(playerSelection)-1)%3) {
+        return("It's a win!")
+    }
+    else return("Try again")
+}
+
+
+
+for (let index = 0; index < 5; index++) {
+    playerSelection = prompt("Your turn").toLowerCase()
+    computerSelection = computerPlay()
+    console.log(getResult(playerSelection, computerSelection))
+    console.log(playerSelection,options[computerSelection])
+}
