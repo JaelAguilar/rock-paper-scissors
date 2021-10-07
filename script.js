4405dd156df8bcbae3f13c9c8c1255cabffcb928
Jael Aguilar <cjaelaguilar@gmail.com>
2021-10-07 11:39:39 -0500

    Deleted console logs and added some documentation

--- script.js
+++ script.js
@@ -43,7 +43,10 @@ function chooseOption(choosed) {
     })
     
 }
-
+/**
+ *Shows all of the cards
+ *
+ */
 function returnCards() {
     userOptions.forEach(option => {
         option.style.display="flex"
@@ -51,7 +54,7 @@ function returnCards() {
 }
 
 /**
- *
+ *Chooses a number and shows the corresponding card
  *
  * @return {int} The random result from 0 to 2 that corresponds to the choosed option of the computer
  */
@@ -75,55 +78,43 @@ function playRound(playerSelection, computerSelection) {
     /*playerSelection is turned to a number between 0 and 2, if commputerSelection is the same, it's a tie, if it is the next, it's a lost, and if it is the previous, it's a win :)*/
     //Tie
     if (computerSelection == playerSelection) {
-        console.log(messages[2]);
         return { messageIndex:2, playerPoint:0, computerPoint:0};
     }
     //Player lost
     else if (computerSelection == (playerSelection + 4) % 3) {
-
         return {messageIndex:1, playerPoint:0, computerPoint:1};
     }
     //Player wins
     else if (computerSelection == (playerSelection + 2) % 3) {
-
         return {messageIndex:0, playerPoint:1, computerPoint:0};
     }
     //Invalid 
         
     else {
-
         return {messageIndex:3, playerPoint:0, computerPoint:0};
     }
 }
 
+/**
+ *Add the points to the score0
+ *
+ */
 function game() {
 
     if (playerScore >= 4) {
-        console.log("end, somebody won");
         DOCscore.innerHTML = "<h1>YOU WIN</h1>"
     }
     else if (computerScore >= 4) {
-      console.log("end, somebody won")
       DOCscore.innerHTML = "<h1>YOU LOSE</h1>"
     }
     else {
       indexComputerSelection = computerPlay()
-      console.log("computer selection: ", indexComputerSelection)
-      console.log("player selection:", indexPlayerSelection)
+
       let gameResult = playRound(indexPlayerSelection, indexComputerSelection)
 
       playerScore += gameResult.playerPoint
       computerScore += gameResult.computerPoint
-      console.log("player: ", playerScore)
-      console.log("computer: ", computerScore)
       DOCscore.innerHTML = "<h1>"+playerScore + " - " + computerScore+"</h1>"
-
-      console.log("..", messages[gameResult.messageIndex])
-      console.log(gameResult.messageIndex)
-      console.log(
-        options[indexPlayerSelection],
-        options[indexComputerSelection]
-      )
     }
 }
 
